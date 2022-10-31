4036e36b2cec48911a1fc6fc775fe2fe2b648413
Kai Morris <kaij.morris@yahoo.com>
2022-10-31 12:44:13 -0400

    finished exercise 2

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -21,7 +21,36 @@ namespace Inheritance
             // Set this class to inherit from your Animal Class
 
 
+            var myBird = new Bird();
+            myBird.WingColor = "green";
+            myBird.CanFly = true;
+            myBird.WillMigrate = true;
+            myBird.BeakLength = 3.5;
 
+            var lizard = new Reptile()
+            {
+                IsColdBlooded = true,
+                IsScaly = true,
+                Habitat = "swamp",
+                CanGrowTail = true,
+            };
+
+            var myAnimals = new Animal[] { myBird, lizard };
+
+            foreach( var animal in myAnimals)
+            {
+                Console.WriteLine($"Alive: {animal.IsAlive}");
+                Console.WriteLine($"Age:{animal.Age} years old");
+                Console.WriteLine($"It has: {animal.LegCount} legs");
+                Console.WriteLine($"It lives by {animal.LandSeaAir}");
+                Console.WriteLine($"");
+            }
+
+
+            {
+
+
+            };
 
             /*Create an object of your Bird class
              *  give values to your members using the object of your Bird class
---
+++ Inheritance/Reptile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Inheritance
+{ 
+    public class Reptile : Animal
+
+    { 
+
+    public Reptile()
+    {
+    }
+
+    public bool IsColdBlooded { get; set; }
+    public bool IsScaly { get; set; }
+    public string Habitat { get; set; }
+    public bool CanGrowTail { get; set;  }
+
+    }
+}
+
